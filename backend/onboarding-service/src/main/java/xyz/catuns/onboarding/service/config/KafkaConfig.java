02834904260c98eccea08713ade39c6335d16213
Devin Catuns <devin@evaitcs.com>
2026-05-25 22:37:17 -0400

    EDP-282 Create KafkaConfig in onboarding-service with producer, consumer, and topic declarations

--- backend/onboarding-service/src/main/java/xyz/catuns/onboarding/service/config/KafkaConfig.java
+++ backend/onboarding-service/src/main/java/xyz/catuns/onboarding/service/config/KafkaConfig.java
@@ -1,34 +1,116 @@
 package xyz.catuns.onboarding.service.config;
 
-import java.util.Map;
-
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.TopicConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import xyz.catuns.onboarding.service.config.properties.AppProperties;
+import xyz.catuns.spring.base.properties.KafkaTopicProperties;
+
+import java.util.Map;
 
 @Configuration
 class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    @Value("${app.kafka.topics.id-correlation}")
+    private String idCorrelationTopicName;
 
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
+    @Value("${app.kafka.topics.github-provisioning}")
+    private String githubProvisioningTopicName;
+
+    @Value("${app.kafka.topics.atlassian-provisioning}")
+    private String atlassianProvisioningTopicName;
+
+    @Value("${app.kafka.topics.onboarding-lifecycle}")
+    private String onboardingLifecycleTopicName;
+
+    @Value("${spring.kafka.properties.schema-registry-url:http://localhost:9090}")
+    private String schemaRegistryUrl;
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory(KafkaProperties kafkaProperties) {
-        Map<String, Object> config = kafkaProperties.buildProducerProperties();
-        // additional ProducerFactory properties;
+    NewTopic idCorrelationTopic(AppProperties appProperties) {
+        KafkaTopicProperties tp = appProperties.getKafka();
+        return TopicBuilder.name(idCorrelationTopicName)
+                .replicas(tp.getReplicas())
+                .partitions(tp.getPartitions())
+                .build();
+    }
+
+    @Bean
+    NewTopic githubProvisioningTopic(AppProperties appProperties) {
+        KafkaTopicProperties tp = appProperties.getKafka();
+        return TopicBuilder.name(githubProvisioningTopicName)
+                .replicas(tp.getReplicas())
+                .partitions(tp.getPartitions())
+                .build();
+    }
+
+    @Bean
+    NewTopic atlassianProvisioningTopic(AppProperties appProperties) {
+        KafkaTopicProperties tp = appProperties.getKafka();
+        return TopicBuilder.name(atlassianProvisioningTopicName)
+                .replicas(tp.getReplicas())
+                .partitions(tp.getPartitions())
+                .build();
+    }
+
+    @Bean
+    NewTopic onboardingLifecycleTopic(AppProperties appProperties) {
+        KafkaTopicProperties tp = appProperties.getKafka();
+        return TopicBuilder.name(onboardingLifecycleTopicName)
+                .replicas(tp.getReplicas())
+                .partitions(tp.getPartitions())
+                .config(TopicConfig.RETENTION_MS_CONFIG, "1209600000") // 14 days
+                .build();
+    }
+
+    @Bean
+    public ProducerFactory<String, SpecificRecord> producerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> config = kafkaProperties.buildProducerProperties(null);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        config.put("schema.registry.url", schemaRegistryUrl);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+    public KafkaTemplate<String, SpecificRecord> kafkaTemplate(ProducerFactory<String, SpecificRecord> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
 
+    @Bean
+    public ConsumerFactory<String, SpecificRecord> consumerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> config = kafkaProperties.buildConsumerProperties(null);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        config.put("schema.registry.url", schemaRegistryUrl);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, SpecificRecord> kafkaListenerContainerFactory(
+            ConsumerFactory<String, SpecificRecord> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, SpecificRecord> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+        return factory;
+    }
 }
